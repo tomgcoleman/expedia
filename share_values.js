14e35074d0c1dc48527908f3e4992f09559f5d00
Tom Coleman <tocoleman@expedia.com>
2016-12-01 08:25:04 -0800

    add file to publish values

--- share_values.js
+++ share_values.js
@@ -5,4 +5,13 @@ if (analyticsdatalayer && analyticsdatalayer.proctor) {
 
     console.log('analyticsdatalayer = ' + analyticsdatalayer);
     console.log('proctor = ' + window.proctor);
+
+    var proctor_share = document.createElement('div');
+    proctor_share.id = 'proctor_share';
+    proctor_share.className = window.proctor;
+    document.body.appendChild(proctor_share);
+
+    console.log('proctor value in element with id:  proctor_share');
+    var element = document.getElementById('proctor_share');
+    console.log(element);
 }
