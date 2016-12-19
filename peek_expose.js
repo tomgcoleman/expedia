65dc08cc996f224cc182d92ad88c3931e1fd726f
Tom Coleman <tocoleman@expedia.com>
2016-12-19 09:09:27 -0800

    update peek script

--- peek_expose.js
+++ peek_expose.js
@@ -3,5 +3,12 @@ if (socket) {
 
     window.peek_socket = socket;
 } else {
+
     console.log('peek expose : socket does not exists');
 }
+
+function get_peek_socket() {
+    return socket;
+}
+
+window.get_peek_socket = get_peek_socket;
