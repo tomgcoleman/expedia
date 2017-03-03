37b8c3cb6706f3c130b54b55e74ec1c618ebaf0f
Tom Coleman <tocoleman@expedia.com>
2017-03-03 14:28:45 -0800

    add a few test cases

--- known_issues.js
+++ known_issues.js
@@ -1,5 +1,6 @@
 {
-    "known_issues": [
+    "known_issues":
+    [
         {
             "description": "git Bathtub in room filter checkbox has no omniture Literal Match",
             "textMatch": "Omniture value for filter #amenitiesContainer, option 1 is HOT:SR:HotelPreference (found undefined)",
@@ -10,6 +11,24 @@
             "description": "git Bathtub in room filter checkbox has no omniture Regex Match",
             "regexMatch": "Omniture.*?option 1 is HOT:SR:HotelPreference.*?undefined",
             "bug": "https://jira/jira/browse/BHS-8150"
+        }
+        ,
+        {
+            "description": "git sponsored listings are missing the map link",
+            "regexMatch": "For hotel.*Map link must be visible.",
+            "bug": "https://jira/jira/browse/BHS-9431"
+        }
+        ,
+        {
+            "description": "git Large footer ads should be visible in breakpoint: 1251, but actually is hidden",
+            "textMatch": "Large footer ads should be visible in breakpoint: 1251, but actually is hidden",
+            "bug": "https://jira/jira/browse/BHS-9341"
+        }
+        ,
+        {
+            "description": "git: For hotel X Map link must be visible.",
+            "regexMatch": "For hotel .* Map link must be visible.",
+            "bug": "https://jira/jira/browse/BHS-9342"
         }
     ]
 }
