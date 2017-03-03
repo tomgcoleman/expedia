0c3e2ea44110080d30b0fbcc93e194e17b0ee492
Tom Coleman <tocoleman@expedia.com>
2017-03-03 10:17:12 -0800

    add known issues file

---
+++ known_issues.js
@@ -0,0 +1,15 @@
+{
+    [
+        {
+            "description": "git Bathtub in room filter checkbox has no omniture Literal Match",
+            "textMatch": "Omniture value for filter #amenitiesContainer, option 1 is HOT:SR:HotelPreference (found undefined)",
+            "bug": "https://jira/jira/browse/BHS-8150"
+        }
+        ,
+        {
+            "description": "git Bathtub in room filter checkbox has no omniture Regex Match",
+            "regexMatch": "Omniture.*?option 1 is HOT:SR:HotelPreference.*?undefined",
+            "bug": "https://jira/jira/browse/BHS-8150"
+        }
+    ]
+}
