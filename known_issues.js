108065237ecac5ade65187cad95d61f515add6bc
Tom Coleman <tocoleman@expedia.com>
2017-03-06 09:04:02 -0800

    add more bugs for initial testing of known as bug

--- known_issues.js
+++ known_issues.js
@@ -54,6 +54,83 @@
             "regexMatch": "Page title phone number is visible",
             "bug": "https://jira/jira/browse/BHS-9367"
         }
-
+        ,
+        {
+            "description": "git: Sort bar must be visible",
+            "regexMatch": "Sort bar .* must be visible",
+            "bug": "https://jira/jira/browse/BHS-9368"
+        }
+        ,
+        {
+            "description": "git: EDS modal issues when date range too large",
+            "regexMatch": "EDS .* must be visible",
+            "bug": "https://jira/jira/browse/BHS-9369"
+        }
+        ,
+        {
+            "description": "git: Distance info must be visible",
+            "regexMatch": "For hotel .* Distance info must be visible",
+            "bug": "https://jira/jira/browse/BHS-9370"
+        }
+        ,
+        {
+            "description": "git: Distance sort must appear for search type",
+            "regexMatch": "Distance sort must appear for search type",
+            "bug": "https://jira/jira/browse/BHS-9371"
+        }
+        ,
+        {
+            "description": "git: The check-out date should store in sessionStorage",
+            "regexMatch": "The check-out date should store in sessionStorage",
+            "bug": "https://jira/jira/browse/BHS-9372"
+        }
+        ,
+        {
+            "description": "git: HSR wizard with in valid dates has many new unit errors",
+            "regexMatch": "Hotel icon in wizard summary must be visible",
+            "bug": "https://jira/jira/browse/BHS-9373"
+        }
+        ,
+        {
+            "description": "git: HSR wizard with in valid dates has many new unit errors",
+            "regexMatch": ".* in wizard summary must be visible",
+            "bug": "https://jira/jira/browse/BHS-9373"
+        }
+        ,
+        {
+            "description": "git: HSR wizard with in valid dates has many new unit errors",
+            "regexMatch": "Search wizard .* glass icon is visible",
+            "bug": "https://jira/jira/browse/BHS-9373"
+        }
+        ,
+        {
+            "description": "git: HSR wizard with in valid dates has many new unit errors",
+            "regexMatch": "The 'data-track' attribute of change search button should be 'HOT.SR.UpdateSearch'",
+            "bug": "https://jira/jira/browse/BHS-9373"
+        }
+        ,
+        {
+            "description": "git: HSR wizard with in valid dates has many new unit errors",
+            "regexMatch": "Check .* date's day of week in wizard summary must be visible.",
+            "bug": "https://jira/jira/browse/BHS-9373"
+        }
+        ,
+        {
+            "description": "git: HSR wizard with in valid dates has many new unit errors",
+            "regexMatch": "Room counts in wizard summary must be visible.",
+            "bug": "https://jira/jira/browse/BHS-9373"
+        }
+        ,
+        {
+            "description": "git: HSR wizard with in valid dates has many new unit errors",
+            "regexMatch": "Check in date should be:.*, actually is: .*",
+            "bug": "https://jira/jira/browse/BHS-9373"
+        }
+        ,
+        {
+            "description": "git: ESR disclaimer must be visible.",
+            "regexMatch": "ESR disclaimer must be visible.",
+            "bug": "https://jira/jira/browse/BHS-9374"
+        }
     ]
 }
