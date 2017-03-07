78a60347aae432b204837537f315ecedd6c7ae40
Tom Coleman <tocoleman@expedia.com>
2017-03-07 10:33:11 -0800

    add more bugs

--- known_issues.js
+++ known_issues.js
@@ -2,25 +2,25 @@
     "known_issues":
     [
         {
-            "description": "git Bathtub in room filter checkbox has no omniture Literal Match",
+            "description": "git: Bathtub in room filter checkbox has no omniture Literal Match",
             "textMatch": "Omniture value for filter #amenitiesContainer, option 1 is HOT:SR:HotelPreference (found undefined)",
             "bug": "https://jira/jira/browse/BHS-8150"
         }
         ,
         {
-            "description": "git Bathtub in room filter checkbox has no omniture Regex Match",
+            "description": "git: Bathtub in room filter checkbox has no omniture Regex Match",
             "regexMatch": "Omniture.*?option 1 is HOT:SR:HotelPreference.*?undefined",
             "bug": "https://jira/jira/browse/BHS-8150"
         }
         ,
         {
-            "description": "git sponsored listings are missing the map link",
+            "description": "git: sponsored listings are missing the map link",
             "regexMatch": "For hotel.*Map link must be visible.",
             "bug": "https://jira/jira/browse/BHS-9431"
         }
         ,
         {
-            "description": "git Large footer ads should be visible in breakpoint: 1251, but actually is hidden",
+            "description": "git: Large footer ads should be visible in breakpoint: 1251, but actually is hidden",
             "textMatch": "Large footer ads should be visible in breakpoint: 1251, but actually is hidden",
             "bug": "https://jira/jira/browse/BHS-9341"
         }
@@ -150,6 +150,17 @@
             "regexMatch": "The guest rating sort is incorrect",
             "bug": "https://jira/jira/browse/BHS-9399"
         }
-
+        ,
+        {
+            "description": "git: Filter orientation pill should match the selected filter state",
+            "regexMatch": "Filter orientation .* has a pill .* which matches the selected filter state",
+            "bug": "https://jira/jira/browse/BHS-9402"
+        }
+        ,
+        {
+            "description": "git: The number of rooms should match with expected value",
+            "regexMatch": "The number of rooms should match with expected value",
+            "bug": "https://jira/jira/browse/BHS-9403"
+        }
     ]
 }
