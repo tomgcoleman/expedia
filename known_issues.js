325e06d0ce287ea6d7e991cafcfda6e5a01710d4
tocoleman <tocoleman@expedia.com>
2017-04-26 10:40:14 -0700

    new bugs in prod

--- known_issues.js
+++ known_issues.js
@@ -426,6 +426,12 @@
         ,
         {
             "description": "git: missing filters should not be checked",
+            "regexMatch": "Price filter .* must be visible.",
+            "bug": "https://jira/jira/browse/BHS-9957"
+        }
+        ,
+        {
+            "description": "git: missing filters should not be checked",
             "regexMatch": "Star rating filter check box must be visible",
             "bug": "https://jira/jira/browse/BHS-9957"
         }
